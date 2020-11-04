7e13e1c285c2255607afb25aef30a8843a5d898c
Kristoffer Baek <kristofferbaek@Kristoffers-iMac.local>
2020-11-04 14:39:01 +0100

    Update plots

--- code/English_plots.R
+++ code/English_plots.R
@@ -422,23 +422,37 @@ axis(side = 4, col.axis = "black", las = 1, cex.axis = cex_axis, at = seq(0, max
 axis(side = 2, col.axis = "black", las = 1, cex.axis = cex_axis, at = seq(0, max_pos + 100, by = 200), labels = seq(0, max_pos + 100, by = 200))
 
 
-arrows(as.Date("2020-08-22"), 260, as.Date("2020-08-22"), 120, lwd = 1, lend = 1, length = 0.1)
-text(as.Date("2020-08-22"), 310, "Mask mandate\npublic transport", cex = 0.9)
 
-arrows(as.Date("2020-09-09"), 500, as.Date("2020-09-09"), 400, lwd = 1, lend = 1, length = 0.1)
-text(as.Date("2020-09-08"), 470, "Restrictions night life\nCopenhagen/Odense", cex = 0.9, adj = 1)
 
-arrows(as.Date("2020-09-18"), 650, as.Date("2020-09-18"), 550, lwd = 1, lend = 1, length = 0.1)
-text(as.Date("2020-09-17"), 620, "Restrictions\nrestaurants etc.", cex = 0.9, adj = 1)
+y <- 60
+arrows(as.Date("2020-07-07"), y + max_pos/14, as.Date("2020-07-07"), y, lwd = 1, lend = 1, length = 0.1)
+text(as.Date("2020-07-08"), y + max_pos/16, "Gathering limit: 100", cex = 0.9, adj = 0)
 
-arrows(as.Date("2020-09-26"), 710, as.Date("2020-09-26"), 610, lwd = 1, lend = 1, length = 0.1)
-text(as.Date("2020-09-27"), 690, "Restrictions\nprivate parties", cex = 0.9, adj = 0)
+y <- 170
+arrows(as.Date("2020-08-14"), y + max_pos/13, as.Date("2020-08-14"), y, lwd = 1, lend = 1, length = 0.1)
+text(as.Date("2020-08-13"), y + max_pos/15, "Extended closing hrs", cex = 0.9, adj = 1)
 
-arrows(as.Date("2020-07-07"), 160, as.Date("2020-07-07"), 60, lwd = 1, lend = 1, length = 0.1)
-text(as.Date("2020-07-08"), 150, "Gathering limit: 100", cex = 0.9, adj = 0)
+y <- 120
+arrows(as.Date("2020-08-22"), y + max_pos/7, as.Date("2020-08-22"), y, lwd = 1, lend = 1, length = 0.1)
+text(as.Date("2020-08-22"), y + max_pos/5, "Mask mandate\npublic transport", cex = 0.9)
 
-arrows(as.Date("2020-08-14"), 220, as.Date("2020-08-14"), 170, lwd = 1, lend = 1, length = 0.1)
-text(as.Date("2020-08-13"), 210, "Extended closing hrs", cex = 0.9, adj = 1)
+y <- 400
+arrows(as.Date("2020-09-09"), y + max_pos/7, as.Date("2020-09-09"), y, lwd = 1, lend = 1, length = 0.1)
+text(as.Date("2020-09-08"), y + max_pos/10, "Restrictions night life\nCopenhagen/Odense", cex = 0.9, adj = 1)
+
+y <- 600
+arrows(as.Date("2020-09-18"), y + max_pos/7, as.Date("2020-09-18"), y, lwd = 1, lend = 1, length = 0.1)
+text(as.Date("2020-09-17"), y + max_pos/10, "Restrictions\nrestaurants etc.", cex = 0.9, adj = 1)
+
+y <- 680
+arrows(as.Date("2020-09-26"), y + max_pos/7, as.Date("2020-09-26"), y, lwd = 1, lend = 1, length = 0.1)
+text(as.Date("2020-09-27"), y + max_pos/10, "Restrictions\nprivate parties", cex = 0.9, adj = 0)
+
+y <- 1230
+arrows(as.Date("2020-10-29"), y + max_pos/7, as.Date("2020-10-29"), y, lwd = 1, lend = 1, length = 0.1)
+text(as.Date("2020-10-28"), y + max_pos/10, "Masks all public indoors,\ngathering limit 10 etc.", cex = 0.9, adj = 1)
+
+
 
 
 legend("topleft",
