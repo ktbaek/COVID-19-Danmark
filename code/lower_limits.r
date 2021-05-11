ceff0a8617eea396b1b55e1072a7cb7f62ea02d9
Kristoffer Baek <kristofferbaek@Kristoffers-iMac.local>
2021-05-11 15:59:34 +0200

    Update plots

--- code/lower_limits.r
+++ code/lower_limits.r
@@ -58,4 +58,43 @@ p1 <- plot_data %>%
   
 ggsave("../figures/ntl_admit_death_expected.png", width = 20, height = 11, units = "cm", dpi = 300)
 
+plot_data %>% 
+  filter(name %in% c("Positive", "Admitted", "Tested")) %>% 
+  pivot_wider(names_from = name, values_from = c(daily, ra), names_sep = "_") %>% 
+  rename(
+    obs_admit_y= ra_Admitted
+  ) %>% 
+  mutate(
+    pool_14 = rollsum(daily_Positive, 14, align = "right", na.pad = TRUE),
+    pred_admit_lo = (pool_14 * 2500 / 5800000) + (2500 * ra(daily_Positive / daily_Tested) * 0.7),
+    pred_admit_hi = (pool_14 * 2500 / 5800000) + (3000 * ra(daily_Positive / daily_Tested) * 1.5)
+  ) %>% 
+  select(Date, obs_admit_y, pred_admit_lo, pred_admit_hi) %>% 
+  pivot_longer(-Date, names_to = c("type", "variable", "limit"), names_sep = "_") %>% 
+  pivot_wider(names_from = limit, values_from = value) %>% 
+  filter(Date >= ymd("2021-02-01")) %>% 
+  ggplot() +
+  geom_ribbon(aes(Date, ymin = lo, ymax = hi, fill = type), alpha = 0.8) +
+  geom_line(aes(Date, y, color = type), size = 1)+#, alpha = type, size = type)) +
+  scale_x_date(labels = my_date_labels, breaks = "1 months", minor_break = "1 month") +
+  scale_y_continuous(limits = c(0, NA)) +
+  labs(
+    x = "Dato", 
+    y = "Antal", 
+    title = "<b><span style = 'font-size:13pt'>Nedre grænse for antal COVID-19 nyindlæggelser</span></b><br><br>Fordi antallet af COVID-19 indlæggelser kun er baseret på en positiv SARS-CoV-2 test, kan der registreres nyindlagte selv i et scenarie hvor ingen indlægges pga. COVID-19.<br><br>De <b style='color:#FC8D62;'>nedre grænser</b> er beregnet udfra antal PCR positive, den gennemsnitlige statistiske risiko for indlæggelse, og metoden hvormed COVID-19 indlæggelser opgøres.<br><br>Estimeret nedre grænse for indlagte viser et interval mellem et muligt lavt og højt scenarie<br>", 
+    caption = "Kristoffer T. Bæk, covid19danmark.dk, datakilde: SSI") +
+  scale_colour_brewer(palette = "Set2", name = "", guide = FALSE) +
+  scale_fill_brewer(palette = "Set2", name = "", 
+                    labels = c("Observeret 7-dages gennemsnit", "Estimeret nedre grænse")) +
+  standard_theme +
+  theme(
+    plot.title.position = "plot",
+    plot.title = element_textbox_simple(
+      size = 10, face = "plain", lineheight = 1.0.5, padding = margin(0, 5, 5, 0)
+    )
+  )
+
+ggsave("../figures/ntl_admit_expected.png", width = 18, height = 10, units = "cm", dpi = 300)
+
+
 
