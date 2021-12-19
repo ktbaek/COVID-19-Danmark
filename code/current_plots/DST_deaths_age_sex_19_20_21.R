c5b48853197580ab794ff67c43c077afc47c6615
ktbaek <kristofferbaek@gmail.com>
2021-12-19 22:44:59 +0100

    Update plots

--- code/current_plots/DST_deaths_age_sex_19_20_21.R
+++ code/current_plots/DST_deaths_age_sex_19_20_21.R
@@ -78,7 +78,7 @@ all_data <- read_csv2("../data/DST_deaths_age_sex.csv", col_names = FALSE) %>%
   distinct() %>%
   mutate(
     Year = as.integer(year(Date)),
-    New_date = `year<-`(Date, 2021)
+    New_date = `year<-`(Date, 2020)
   ) %>%
   pivot_longer(c(Male, Female), names_to = "Sex", values_to = "Daily") %>%
   mutate(Quarter = quarter(Date)) %>%
@@ -90,32 +90,26 @@ all_data %>%
   rename(Daily_deaths = Daily) %>%
   write_csv2("../data/tidy_dst_age_sex_2015_21.csv")
 
+all_data$Age <- factor(all_data$Age, levels = c(
+  "0-4", "5-9", "10-14", "15-19", "20-24", "25-29", "30-34", "35-39",
+  "40-44", "45-49", "50-54", "55-59", "60-64", "65-69", "70-74", "75-79",
+  "80-84", "85-89", "90-94", "95-99", "100+"
+))
+
 plot_data <- all_data %>%
   mutate(Daily_relative = Daily / Population * 100000) %>% 
   group_by(Year_group = ifelse(Year %in% c(2015:2019), "2015-2019", as.character(Year)), Age, Sex, New_date) %>%
   summarize(
-    Daily_max = max(Daily, na.rm = TRUE),
-    Daily_min = min(Daily, na.rm = TRUE),
-    Daily_relative_max = max(Daily_relative, na.rm = TRUE),
-    Daily_relative_min = min(Daily_relative, na.rm = TRUE),
     Daily = mean(Daily, na.rm = TRUE),
     Daily_relative = mean(Daily_relative, na.rm = TRUE)) %>% 
   group_by(Year_group, Age, Sex) %>% 
   arrange(New_date) %>%
   mutate(
-    Cum_max = cumsum(replace_na(Daily_max, 0)),
-    Cum_min = cumsum(replace_na(Daily_min, 0)),
-    Cum_relative_max = cumsum(replace_na(Daily_relative_max, 0)),
-    Cum_relative_min = cumsum(replace_na(Daily_relative_min, 0)),
     Cum = cumsum(replace_na(Daily, 0)),
     Cum_relative = cumsum(replace_na(Daily_relative, 0)),
   )
 
-plot_data$Age <- factor(plot_data$Age, levels = c(
-  "0-4", "5-9", "10-14", "15-19", "20-24", "25-29", "30-34", "35-39",
-  "40-44", "45-49", "50-54", "55-59", "60-64", "65-69", "70-74", "75-79",
-  "80-84", "85-89", "90-94", "95-99", "100+"
-))
+
 
 plot_layer <- list(
   scale_x_date(date_labels = "%e %b", date_breaks = "4 months", minor_breaks = "1 month"),
@@ -194,3 +188,91 @@ plot_data %>%
   )
 
 ggsave("../figures/DST_deaths_19_20_21/dst_deaths_age_sex_cum_rel_old.png", width = 18, height = 10, units = "cm", dpi = 300)
+
+all_data %>% 
+  group_by(Date, Year, Age) %>% 
+  summarize(Population = sum(Population)) %>% 
+  ggplot() +
+  geom_area(aes(Date, Population, fill = Age, color = Age), alpha = 0.4, size = 0.6, position = "stack") +
+  scale_x_date(date_label = "%Y", date_breaks = "3 years", minor_breaks = "1 year") +
+  facet_wrap(~ Age, ncol = 7) +
+  scale_y_continuous(labels = scales::number) +
+  labs(
+    title = "Befolkningsudvikling 2015-2021",
+    caption = "Kristoffer T. Bæk, data: Danmarks Statistik"
+  ) +
+  facet_theme +
+  theme(
+    legend.position = "none",
+    panel.grid.minor.x = element_line(size = rel(0.3)),
+    plot.margin = margin(0.8, 0.8, 0.3, 0.8, "cm"),
+    axis.title.y = element_blank()
+  )
+
+ggsave("../figures/DST_deaths_19_20_21/dst_pop_age_2015_21.png", width = 18, height = 10, units = "cm", dpi = 300)
+
+
+# all_data$Age <- factor(all_data$Age, levels = rev(levels(all_data$Age)))
+# 
+# p2 <- all_data %>% 
+#   group_by(Date, Year, Age) %>% 
+#   summarize(Population = sum(Population)) %>% 
+#   ggplot() +
+#   geom_area(aes(Date, Population, fill = Age, color = Age), alpha = 0.6, position = "fill") +
+#   scale_x_date(date_label = "%Y", date_breaks = "3 years", minor_breaks = "1 year") +
+#   scale_fill_manual(values = rev(hue_pal()(21))) +
+#   scale_color_manual(values = rev(hue_pal()(21))) +
+#   scale_y_continuous(labels = function(x) paste0(x * 100, " %")) +
+#   facet_theme +
+#   theme(
+#     legend.position = "none",
+#     axis.title.y = element_blank(),
+#     panel.grid.minor.x = element_line(size = rel(0.3))
+#   )
+# 
+# p1 + p2 +
+#   plot_layout(widths = c(4, 1))
+
+
+
+
+
+all_data %>%
+  group_by(Year, Date) %>% 
+  summarize(
+    Population = sum(Population, na.rm = TRUE),
+    Daily = sum(Daily, na.rm = TRUE),
+    Daily_relative = Daily / Population * 100000
+  ) %>% 
+  mutate(
+    New_date = `year<-`(Date, 2020)
+  ) %>% 
+  group_by(Year_group = ifelse(Year %in% c(2015:2019), "2015-2019", as.character(Year)), New_date) %>%
+  summarize(
+    Daily = mean(Daily, na.rm = TRUE),
+    Daily_relative = mean(Daily_relative, na.rm = TRUE)
+  ) %>% 
+  arrange(New_date) %>%
+  mutate(
+    Cum = cumsum(replace_na(Daily, 0)),
+    Cum_relative = cumsum(replace_na(Daily_relative, 0)),
+  ) %>% 
+  pivot_longer(c(Cum, Cum_relative)) %>% 
+  ggplot() +
+  geom_line(aes(New_date, value, color = as.factor(Year_group))) +
+  scale_x_date(date_labels = "%e %b", date_breaks = "3 months", minor_breaks = "1 month") +
+scale_color_manual(name = "", values = rev(hue_pal()(3))) +
+guides(color = guide_legend(override.aes = list(size = 1))) +
+  facet_wrap(~ name, labeller = labeller(name = c("Cum" = "Antal døde", "Cum_relative" = "Antal døde per 100.000")), scales = "free") +
+facet_theme +
+theme(
+  axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1),
+  plot.margin = margin(0.6, 0.6, 0.3, 0.6, "cm"),
+  panel.grid.minor.x = element_blank()
+) +
+  labs(
+    y = "Cumulated deaths per 100,000",
+    caption = "Kristoffer T. Bæk, data: Danmarks Statistik"
+  )
+
+ggsave("../figures/DST_deaths_19_20_21/dst_deaths_cum_rel_all.png", width = 18, height = 10, units = "cm", dpi = 300)
